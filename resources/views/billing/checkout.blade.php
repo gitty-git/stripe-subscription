ee72969a3c4b6fefb5082dbd617696f45cc26429
Evgeny P <livin9000@gmail.com>
2021-09-22 02:32:09 +0500

    Stripe form colours

--- resources/views/billing/checkout.blade.php
+++ resources/views/billing/checkout.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 
 <div class="relative text-white flex items-top justify-center min-h-screen bg-gray-900 sm:items-center py-4 sm:pt-0 font-mono">
-    <form action="{{ route('checkout.process') }}" method="POST" id="checkout_form" class="w-96">
+    <form action="{{ route('checkout.process') }}" method="POST" id="checkout-form" class="w-96">
         @csrf
-        <div class="pb-4 text-red-300 text-sm" id="payment_errors"></div>
+        <div class="pb-4 text-red-300 text-sm" id="payment-errors"></div>
 
         <input type="hidden" name="billing_plan_id" value="{{ $plan->id }}" />
 
-        <input type="hidden" name="payment_method" id="payment_method" value="">
+        <input type="hidden" name="payment-method" id="payment-method" value="">
 
         <input id="card-holder-name" type="text" class="bg-gray-900 mb-8 w-full p-2 border-2 border-gray-800 rounded-xl hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50">
 
@@ -20,4 +20,58 @@
         </button>
     </form>
 </div>
+
+<script src="https://js.stripe.com/v3/"></script>
+
+<script>
+    const stripe = Stripe("{{ env('STRIPE_KEY') }}");
+    const elements = stripe.elements();
+    const cardElement = elements.create('card', {
+        style: {
+            base: {
+                iconColor: 'white',
+                color: '#fff',
+                fontWeight: '500',
+                fontFamily: '"Courier Prime", "Courier New", monospace',
+                fontSize: '16px',
+                fontSmoothing: 'antialiased',
+                ':-webkit-autofill': {
+                    color: '#FDE68A',
+                },
+                '::placeholder': {
+                    color: '#737373',
+                },
+            },
+            invalid: {
+                iconColor: '#FCA5A5',
+                color: '#FCA5A5',
+            },
+        }
+    });
+    cardElement.mount('#card-element');
+    const cardHolderName = document.getElementById('card-holder-name');
+    const cardButton = document.getElementById('card-button');
+    const clientSecret = cardButton.dataset.secret;
+
+    cardButton.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const {setupIntent, error} = await stripe.confirmCardSetup(
+            clientSecret, {
+                payment_method: {
+                    card: cardElement,
+                    billing_details: {
+                        name: cardHolderName.value
+                    }
+                }
+            }
+        );
+
+        if (error) {
+            document.getElementById('payment-errors').innerHTML = error.message;
+        } else {
+            document.getElementById("payment-method").value = setupIntent.payment_method;
+            document.getElementById('checkout-form').submit();
+        }
+    });
+</script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,5 @@
     </div>
 </footer>
 
-<script src="https://js.stripe.com/v3/"></script>
 <script src="{{ asset('js/app.js')}}"></script>
-
 </html>
