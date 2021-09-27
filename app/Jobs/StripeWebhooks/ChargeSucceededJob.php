254972e5720247dbf4bea0339f6dffc05906150e
Evgeny P <livin9000@gmail.com>
2021-09-27 13:30:36 +0500

    SucceedNotification

--- app/Jobs/StripeWebhooks/ChargeSucceededJob.php
+++ app/Jobs/StripeWebhooks/ChargeSucceededJob.php
@@ -4,6 +4,7 @@ namespace App\Jobs\StripeWebhooks;
 
 use App\Models\Payment;
 use App\Models\User;
+use App\Notifications\ChargeSuccessNotification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -28,12 +29,14 @@ class ChargeSucceededJob implements ShouldQueue
         $charge = $this->webhookCall->payload['data']['object'];
         $user = User::where('stripe_id', $charge['customer'])->first();
         if ($user) {
-            Payment::create([
+            $payment = Payment::create([
                 'user_id' => $user->id,
                 'stripe_id' => $charge['id'],
                 'subtotal' => $charge['amount'],
                 'total' => $charge['amount'],
             ]);
+
+            $user->notify(new ChargeSuccessNotification($payment));
         }
     }
 }
---
+++ app/Notifications/ChargeSuccessNotification.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Payment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ChargeSuccessNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(private Payment $payment) {}
+
+    public function via(mixed $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(mixed $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('You have paid $'. number_format($this->payment->total / 100, 0) .'.')
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
